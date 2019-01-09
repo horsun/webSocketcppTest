99740b9f123c55d2a924e70faed1b542501d65b7
Horsun <im7h1995@hotmail.com>
2019-01-09 13:17:55 +0800

    Update 源.cpp

--- webSocketTest/webSocketTest/源.cpp
+++ webSocketTest/webSocketTest/源.cpp
@@ -3,54 +3,77 @@
 #include "websocketpp/config/asio_no_tls.hpp"
 #include "websocketpp/server.hpp"
 #include <stdio.h>
+#include <list>
+
 using namespace std;
 
+
+class MyJson {
+public:
+
+    void writeJson() {
+        // key & value
+        // type list
+        Json::Value root;
+        Json::Value arrayObj;
+        Json::Value item;
+
+        item["cpp"] = "jsoncpp";
+        item["java"] = "jsoninjava";
+        item["php"] = "support";
+        arrayObj.append(item);
+
+        root["name"] = "json";
+        root["array"] = arrayObj;
+/*
+		root.toStyledString();
+		string out = root.toStyledString();*/
+        cout <<   root << endl;
+    };
+    void addObj() {
+
+    };
+    void toString() {
+
+    };
+    void jsonStrReader(string jsonStr) {
+        cout << "鏀跺埌鐨剆tring鏄細" << jsonStr << endl;
+        Json::Value newJson;
+        Json::CharReaderBuilder builder;
+        builder["collectiComments"] = false;
+        JSONCPP_STRING errs;
+        Json::CharReader* reader = builder.newCharReader();
+        if (!reader->parse(jsonStr.data(), jsonStr.data() + jsonStr.size(), &newJson, &errs))//浠巎sonStr涓鍙栨暟鎹埌newJson
+        {
+            return;
+        };
+        jsonStr.clear();
+        cout <<"瑙ｆ瀽鍚庣殑json锛歕n"<< newJson << endl;
+        cout << "json 鐨剈ploadid 鏄細" << newJson["uploadid"] << endl;
+        cout << ">>>>>>>>>>>>>>>" << endl;
+    };
+};
+
+
 typedef websocketpp::server<websocketpp::config::asio>server;
-void on_message(websocketpp::connection_hdl hadl,server::message_ptr msg) {
-	string jsonStr = msg->get_payload();
-	cout << "收到的string是：" << jsonStr << endl;
-	Json::Value newJson;
-	Json::CharReaderBuilder builder;
-	builder["collectiComments"] = false;
-	JSONCPP_STRING errs;
-	Json::CharReader* reader = builder.newCharReader();
-	if (!reader->parse(jsonStr.data(), jsonStr.data() + jsonStr.size(), &newJson, &errs))//从jsonStr中读取数据到newJson
-	{
-		cout << "error" << endl;
-		return;
-	};
-	cout << newJson << std::endl;
-	cout << "json 的code 是：" << newJson["code"] << endl;
-
-	/*printf("msg:",msg->get_payload());
-	std::cout << msg->get_payload() << std::endl;*/
-	return;
+void on_message(websocketpp::connection_hdl hadl, server::message_ptr msg) {
+    string jsonStr = msg->get_payload();
+    MyJson testJSon;
+    testJSon.jsonStrReader(jsonStr);
 };
 
-void jsonStrReader(string jsonStr) {
-	cout << "收到的string是：" << jsonStr << endl;
-	Json::Value newJson;
-	Json::CharReaderBuilder builder;
-	builder["collectiComments"] = false;
-	JSONCPP_STRING errs;
-	Json::CharReader* reader = builder.newCharReader();
-	if (!reader->parse(jsonStr.data(), jsonStr.data() + jsonStr.size(), &newJson, &errs))//从jsonStr中读取数据到newJson
-	{
-		return;
-	};
-	jsonStr.clear();
-	cout << newJson << endl;
-	cout << "json 的uploadid 是：" << newJson["uploadid"] << endl;
-	system("pause");
 
-};
+
 
 void main() {
-	jsonStrReader("{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}");
-	server print_sever;
-	print_sever.set_message_handler(&on_message);
-	print_sever.init_asio();
-	print_sever.listen(9002);
-	print_sever.start_accept();
-	print_sever.run();
+    MyJson json;
+    json.jsonStrReader("{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}");
+    json.writeJson();
+    cout << "鏈嶅姟姝ｅ湪鍚姩 ws://127.0.0.1:9002" << endl;
+    server print_sever;
+    print_sever.set_message_handler(&on_message);
+    print_sever.init_asio();
+    print_sever.listen(9002);
+    print_sever.start_accept();
+    print_sever.run();
 }
